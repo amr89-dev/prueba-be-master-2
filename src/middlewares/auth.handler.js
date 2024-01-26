26483ae32b89df2cdb3806a825d81faf6d31da4b
amr89-dev <amr89.dev@gmail.com>
2024-01-25 20:43:58 -0500

    Refactor auth.handler.js middleware

--- src/middlewares/auth.handler.js
+++ src/middlewares/auth.handler.js
@@ -1,30 +1,8 @@
 const boom = require("@hapi/boom");
 require("dotenv").config();
 
-const { API_KEY } = process.env;
-
-const checkApiKey = (req, res, next) => {
-  const apiKey = req.headers["key"];
-  console.log(API_KEY);
-  if (apiKey === API_KEY) {
-    next();
-  } else {
-    next(boom.unauthorized());
-  }
-};
-
-const checkAdminRole = (req, res, next) => {
-  const user = req.user;
-  if (user.role === "admin") {
-    next();
-  } else {
-    next(boom.unauthorized());
-  }
-};
-
 const checkRoles = (...roles) => {
   return (req, res, next) => {
-    console.log("-->", req.user);
     const user = req.user;
 
     if (roles.includes(user.role)) {
@@ -45,4 +23,4 @@ const checkProfile = (...profiles) => {
     }
   };
 };
-module.exports = { checkApiKey, checkAdminRole, checkRoles };
+module.exports = { checkProfile, checkRoles };
