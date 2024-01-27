2eb07530fbef0dcdeba0020cb0cd5b322f635604
amr89-dev <amr89.dev@gmail.com>
2024-01-27 15:53:06 -0500

    Add comment routes for getting, updating, and deleting comments

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -4,6 +4,8 @@ const CommentService = require("../services/comment.service");
 const validatorHandler = require("../middlewares/validator.handler");
 const {
   createCommentSchema,
+  updateCommentSchema,
+  getCommentSchema,
 } = require("../middlewares/schemas/comments.schema");
 const commentService = new CommentService();
 
@@ -23,6 +25,48 @@ router.post(
     try {
       const comment = await commentService.create(req.body);
       res.status(201).json(comment);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  "/:id",
+  validatorHandler(getCommentSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const comment = await commentService.findOne(id);
+      res.status(200).json(comment);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  validatorHandler(updateCommentSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const comment = await commentService.update(id, req.body);
+      res.status(200).json(comment);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  "/:id",
+  validatorHandler(getCommentSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await commentService.delete(id);
+      res.status(200).json({ message: "Comment deleted" });
     } catch (err) {
       next(err);
     }
